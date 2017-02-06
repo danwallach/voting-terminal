573fb803953a3628d5ad439f63093b88d9bf8a31
Brian Tran <a.brian.tran@gmail.com>
2017-02-06 13:27:42 -0600

    Implement Race component
    Race consists of a name and three Candidate tables for First Choice,
    Second Choice, and Third Choice.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class Candidate extends React.Component {
   render() {
     return (
       <tr>
-        <td style={tdStyle} classname="mdl-data-table__cel--non-numeric">
+        <td style={tdStyle} className="mdl-data-table__cel--non-numeric">
             <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
               <input type="checkbox" className="mdl-checkbox__input"/>
                 <span className="mdl-checkbox__label">{this.props.name}</span>
@@ -19,29 +19,54 @@ class Candidate extends React.Component {
   }
 }
 
-const writeInCandidate = 
+const writeInCandidate =
   <form action="#">
     <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
       <input className="mdl-textfield__input" type="text" id="sample3" />
-      <label className="mdl-textfield__label" for="sample3">Text...</label>
+      <label className="mdl-textfield__label" htmlFor="sample3">Write-in Candidate</label>
     </div>
   </form>
 class CandidateTable extends React.Component {
   render () {
-    var rows = []
-    this.props.candidates.forEach(function(candidate) {
-      rows.push(<Candidate name={candidate} />);
+    var head;
+    switch (this.props.choiceNo) {
+      case 1:
+        head = <tr>
+          <th className="mdl-data-table__cell--non-numberic">
+            First Choice
+            <p>Vote for One</p>
+          </th>
+        </tr>;
+        break;
+      case 2:
+        head = <tr>
+          <th className="mdl-data-table__cell--non-numberic">
+            Second Choice
+            <p>Vote for One: Must be different than your first choice</p>
+          </th>
+        </tr>;
+        break;
+      case 3:
+        head = <tr>
+          <th className="mdl-data-table__cell--non-numberic">
+            Third Choice
+            <p>Vote for One: Must be different than your first and second choices</p>
+          </th>
+        </tr>;
+        break;
+      default:
+    }
+    var rows = [];
+    this.props.candidates.forEach((candidate, index) => {
+      rows.push(<Candidate name={candidate} key={index}/>);
     });
     rows.push(
-      <Candidate name={writeInCandidate} />
+      <Candidate name={writeInCandidate} key={999}/>
     );
     return (
       <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
         <thead>
-          <tr>
-            <th className="mdl-data-table__cell--non-numberic">First Choice
-      <p>Vote for One</p></th>
-          </tr>
+          {head}
         </thead>
         <tbody>
           {rows}
@@ -53,10 +78,27 @@ class CandidateTable extends React.Component {
 
 var CANDIDATES = ["Ocean", "Mountain", "Lake", "Forest", "Beach"];
 
+class Race extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.name}</h2>
+        <p>Vote your first, second, and third choices</p>
+        <CandidateTable candidates={CANDIDATES} choiceNo={1}/>
+        <CandidateTable candidates={CANDIDATES} choiceNo={2}/>
+        <CandidateTable candidates={CANDIDATES} choiceNo={3}/>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <CandidateTable candidates={CANDIDATES} />
+      <div>
+        <h1>City and County</h1>
+        <Race name="Favorite Nature Setting" />
+      </div>
     );
   }
 }
