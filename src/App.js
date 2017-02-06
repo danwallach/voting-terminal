22581063f06e0ea3a1afa0ed73d0b46c5e74f468
Brian Tran <a.brian.tran@gmail.com>
2017-02-06 11:39:00 -0600

    Implement Candidate and CandidateTable components
    CandidateTables are the components corresponding to First Choice, Second
    Choice, and Third Choice sections on ballot.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const tdStyle = {
+  textAlign: "left",
+};
+class Candidate extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td style={tdStyle} classname="mdl-data-table__cel--non-numeric">
+            <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
+              <input type="checkbox" className="mdl-checkbox__input"/>
+                <span className="mdl-checkbox__label">{this.props.name}</span>
+            </label>
+        </td>
+      </tr>
+    );
+  }
+}
+
+const writeInCandidate = 
+  <form action="#">
+    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+      <input className="mdl-textfield__input" type="text" id="sample3" />
+      <label className="mdl-textfield__label" for="sample3">Text...</label>
+    </div>
+  </form>
+class CandidateTable extends React.Component {
+  render () {
+    var rows = []
+    this.props.candidates.forEach(function(candidate) {
+      rows.push(<Candidate name={candidate} />);
+    });
+    rows.push(
+      <Candidate name={writeInCandidate} />
+    );
+    return (
+      <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+        <thead>
+          <tr>
+            <th className="mdl-data-table__cell--non-numberic">First Choice
+      <p>Vote for One</p></th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+var CANDIDATES = ["Ocean", "Mountain", "Lake", "Forest", "Beach"];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <CandidateTable candidates={CANDIDATES} />
     );
   }
 }
