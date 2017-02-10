26c2375f7a3f548298c3009bab4cc3eedd719145
kksgandhi <kksgandhi@gmail.com>
2017-02-10 16:10:43 -0600

    I am currently crying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var CHECKBOXVALUES = [[1, 0, 0, 0, 0, 0],[0,0,0,0,1,0],[0,0,0,0,0,0]]
 const tdStyle = {
   textAlign: "left",
 };
@@ -12,8 +13,13 @@ class Candidate extends React.Component {
       <td nowrap style={tdStyle} className="mdl-data-table__cel--non-numeric">
       <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect">
       <input type="checkbox" className="mdl-checkbox__input"
-      onClick={() => this.props.onClick()}/>
-      <span className="mdl-checkbox__label">{this.props.name}</span>
+      onClick={() => this.props.onClick()}
+      checked={CHECKBOXVALUES[this.props.tableValue][this.props.indexValue]===0? "" : "checked"}
+      />
+      <span className="mdl-checkbox__label">
+      {this.props.name}
+      {CHECKBOXVALUES[this.props.tableValue][this.props.indexValue]}
+      </span>
       </label>
       </td>
       </tr>
@@ -29,26 +35,38 @@ const writeInCandidate =
     </div>
   </form>
 
-var CHECKBOXVALUES = [[1, 0, 0, 0, 0, 0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
 
 class CandidateTable extends React.Component {
   renderCandidate(candidate, index){
     return <Candidate name={candidate}
     key={index}
-    checkValue={CHECKBOXVALUES[index]}
+    indexValue={index}
+    tableValue={this.props.choiceNo-1}
     onClick={() => this.handleClick(index)}
       />;
   }
 
-  handleClick(i){
-    if(CHECKBOXVALUES[this.props.choiceNo-1][i]===1){
-      CHECKBOXVALUES[this.props.choiceNo-1][i]=0
+  handleClick(index){
+    const table = this.props.choiceNo - 1;
+    this.forceUpdate();
+    if(CHECKBOXVALUES[table][index]===1){
+      CHECKBOXVALUES[table][index]=0
     }
     else{
-      CHECKBOXVALUES[this.props.choiceNo-1][i]=1
+      CHECKBOXVALUES[table][index]=1
+    }//TODO NO MAGIC NUMBERS
+    for(let i =0;i < 3; i++){
+      if(i!==table){
+        CHECKBOXVALUES[i][index]=0;
+      }
+    }
+    for(let i = 0; i < 6; i++){
+      if(i!==index){
+        CHECKBOXVALUES[table][i]=0;
+      }
     }
     alert(CHECKBOXVALUES)
-    alert(this.props.choiceNo)
+    this.props.parentRender();
   }
   render () {
     var head;
@@ -64,9 +82,9 @@ class CandidateTable extends React.Component {
     this.props.candidates.forEach((candidate, index) => {
       rows.push(this.renderCandidate(candidate, index));
     });
-    rows.push(
+    /*rows.push(
       <Candidate name={writeInCandidate} key={999} onClick={() => this.handleClick(6)} />
-    );
+    );*/
     return (
       <table style={{float: " left"}} className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
         <thead>
@@ -80,17 +98,22 @@ class CandidateTable extends React.Component {
   }
 }
 
-var CANDIDATES = ["Ocean", "Mountain", "Lake", "Forest", "Beach"];
+var CANDIDATES = ["Ocean", "Mountain", "Lake", "Forest", "Beach","Flarp"];
 
 class Race extends React.Component {
+  reRenderAll(){
+    alert("Updating")
+    //this.forceUpdate();
+    this.forceUpdate();
+  }
   render() {
     return (
       <div>
         <h2>{this.props.name}</h2>
         <p>Vote your first, second, and third choices</p>
-        <CandidateTable candidates={CANDIDATES} choiceNo={1}/>
-        <CandidateTable candidates={CANDIDATES} choiceNo={2}/>
-        <CandidateTable candidates={CANDIDATES} choiceNo={3}/>
+        <CandidateTable parentRender={this.reRenderAll} candidates={CANDIDATES} choiceNo={1}/>
+        <CandidateTable parentRender={this.reRenderAll} candidates={CANDIDATES} chocandidates={CANDIDATES} choiceNo={2}/>
+        <CandidateTable parentRender={this.reRenderAll} candidates={CANDIDATES} choiceNo={3}/>
       </div>
     )
   }
