9fe1e42889faa6246784cd7bd2969ccf88e5c7d9
Brian Tran <a.brian.tran@gmail.com>
2017-02-24 02:39:42 -0600

    Fix material-design-icons import

--- src/SanFranciscoDesign.jsx
+++ src/SanFranciscoDesign.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-//import "../node_modules/roboto-fontface/css/roboto/roboto-fontface.css";
-//import "../node_modules/material-design-icons/";
-//import "./material.css";
-//import "./material.js";
+import "../node_modules/roboto-fontface/css/roboto/roboto-fontface.css";
+import "../node_modules/material-design-icons/iconfont/material-icons.css";
+import "./material.css";
+import "./material.js";
 import "./Office.css";
 import election from "./election.json";
 
