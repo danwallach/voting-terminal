3c3f6731a96a06e86be491df68146b3fc252cba2
Kutub Gandhi <kksgandhi@gmail.com>
2017-03-09 16:58:04 -0600

    add Final page to git

---
+++ src/FinalPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class FinalPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Thank you for voting!
+        </p>
+      </div>
+    );
+  }
+}
+export default FinalPage;
