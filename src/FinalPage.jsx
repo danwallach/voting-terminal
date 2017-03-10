9f0ba30cbd94e8132292a25157e0dfbcf227db1b
Brian Tran <a.brian.tran@gmail.com>
2017-03-09 20:07:22 -0600

    fix(FinalPage): Match Claudia's specifications

--- src/FinalPage.jsx
+++ src/FinalPage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+
 class FinalPage extends React.Component {
   render() {
     return (
-      <div>
-        <p>
-      <font size="30">
-          Thank you for voting!
-      </font>
-        </p>
+      <div style={{ textAlign: "center" }}>
+        <h1 className="mdc-typography--display4 mdc-typography--adjust-margin">
+          You voted!
+        </h1>
+        <h2 className="mdc-typography--display2 mdc-typography--adjust-margin">
+          Thank you!
+        </h2>
       </div>
     );
   }
 }
+
 export default FinalPage;
