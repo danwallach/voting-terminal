95d2ab57d30d03cec602105d83b6ca471325ae29
Brian Tran <a.brian.tran@gmail.com>
2017-03-23 11:11:12 -0500

    feat(final): add hidden button to return to start
    - Konami code is tapping the letter 'e' 5 times

--- src/FinalPage.jsx
+++ src/FinalPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { hashHistory } from "react-router";
 
 class FinalPage extends React.Component {
+  constructor() {
+    super();
+    this.state = { counter: 0 };
+  }
+  handleClick = () => {
+    if (this.state.counter >= 4) {
+      hashHistory.push("/");
+    }
+    this.setState(prevState => ({ counter: prevState.counter + 1 }));
+  };
   render() {
     return (
       <div style={{ textAlign: "center" }}>
         <h1 className="mdc-typography--display4 mdc-typography--adjust-margin">
-          You voted!
+          You vot<span onClick={this.handleClick}>e</span>d!
         </h1>
         <h2 className="mdc-typography--display2 mdc-typography--adjust-margin">
           Thank you!
